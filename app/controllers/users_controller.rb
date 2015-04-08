fed510787e23495ded4ab1245abbbeaffa080ca8
komu <komuw05@gmail.com>
2015-04-08 12:10:32 +0300

    chap11 halfway

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id]) #same as, User.find(1)
+    @microposts = @user.microposts.paginate(page: params[:page])
   end
 
   def register
